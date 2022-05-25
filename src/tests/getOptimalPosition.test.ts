69b3700c853328f0cfbb0410e69cf8456246420b
Donat Avdijaj <donatavdijaj@gmail.com>
2022-05-25 08:23:29 +0200

    Add tests

--- src/tests/getOptimalPosition.test.ts
+++ src/tests/getOptimalPosition.test.ts
@@ -9,3 +9,64 @@ test("Empty grid test", () => {
     })
   ).toStrictEqual({ x: 0, y: 0 });
 });
+
+test("Empty grid with size 0 test", () => {
+  expect(
+    getOptimalPosition({
+      grid: { width: 0, height: 0 },
+      components: [],
+      newElement: { width: 3, height: 3 },
+    })
+  ).toStrictEqual({ x: 0, y: 0 });
+});
+
+test("Filled grid with size 0 test", () => {
+  expect(
+    getOptimalPosition({
+      grid: { width: 0, height: 0 },
+      components: [
+        { id: "a", x: 0, y: 0, width: 6, height: 3 },
+        { id: "b", x: 6, y: 0, width: 6, height: 6 },
+      ],
+      newElement: { width: 3, height: 3 },
+    })
+  ).toStrictEqual({ x: 0, y: 0 });
+});
+
+test("Full grid", () => {
+  expect(
+    getOptimalPosition({
+      grid: { width: 16, height: 8 },
+      components: [{ id: "a", x: 0, y: 0, width: 16, height: 8 }],
+      newElement: { width: 3, height: 3 },
+    })
+  ).toStrictEqual({ x: 16, y: 8 });
+});
+
+test("Regular use #1", () => {
+  expect(
+    getOptimalPosition({
+      grid: { width: 16, height: 8 },
+      components: [
+        { id: "a", x: 0, y: 0, width: 6, height: 3 },
+        { id: "b", x: 6, y: 0, width: 6, height: 6 },
+      ],
+      newElement: { width: 4, height: 7 },
+    })
+  ).toStrictEqual({ x: 12, y: 0 });
+});
+
+test("Regular use #2", () => {
+  expect(
+    getOptimalPosition({
+      grid: { width: 16, height: 8 },
+      components: [
+        { id: "a", x: 0, y: 0, width: 3, height: 7 },
+        { id: "b", x: 3, y: 0, width: 5, height: 2 },
+        { id: "c", x: 3, y: 2, width: 4, height: 2 },
+        { id: "d", x: 10, y: 0, width: 6, height: 6 },
+      ],
+      newElement: { width: 4, height: 7 },
+    })
+  ).toStrictEqual({ x: 3, y: 4 });
+});
