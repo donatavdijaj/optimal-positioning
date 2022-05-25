b2e458cff5a73f3d40f6a605efb5b58099976898
Donat Avdijaj <donatavdijaj@gmail.com>
2022-05-25 08:41:30 +0200

    Add new test and change names

--- src/tests/getOptimalPosition.test.ts
+++ src/tests/getOptimalPosition.test.ts
@@ -1,6 +1,6 @@
 import getOptimalPosition from "../utils/getOptimalPosition";
 
-test("Empty grid test", () => {
+test("Empty grid", () => {
   expect(
     getOptimalPosition({
       grid: { width: 16, height: 8 },
@@ -10,7 +10,7 @@ test("Empty grid test", () => {
   ).toStrictEqual({ x: 0, y: 0 });
 });
 
-test("Empty grid with size 0 test", () => {
+test("Empty grid with size 0", () => {
   expect(
     getOptimalPosition({
       grid: { width: 0, height: 0 },
@@ -20,7 +20,7 @@ test("Empty grid with size 0 test", () => {
   ).toStrictEqual({ x: 0, y: 0 });
 });
 
-test("Filled grid with size 0 test", () => {
+test("Filled grid with size 0", () => {
   expect(
     getOptimalPosition({
       grid: { width: 0, height: 0 },
@@ -40,10 +40,40 @@ test("Full grid", () => {
       components: [{ id: "a", x: 0, y: 0, width: 16, height: 8 }],
       newElement: { width: 3, height: 3 },
     })
-  ).toStrictEqual({ x: 16, y: 8 });
+  ).toStrictEqual({ x: 0, y: 8 });
 });
 
-test("Regular use #1", () => {
+test("New item is wider than grid", () => {
+  expect(
+    getOptimalPosition({
+      grid: { width: 16, height: 8 },
+      components: [
+        { id: "a", x: 0, y: 0, width: 3, height: 7 },
+        { id: "b", x: 3, y: 0, width: 5, height: 2 },
+        { id: "c", x: 3, y: 2, width: 4, height: 2 },
+        { id: "d", x: 10, y: 0, width: 6, height: 6 },
+      ],
+      newElement: { width: 17, height: 2 },
+    })
+  ).toStrictEqual({ x: 0, y: 8 });
+});
+
+test("New item is taller than grid", () => {
+  expect(
+    getOptimalPosition({
+      grid: { width: 16, height: 8 },
+      components: [
+        { id: "a", x: 0, y: 0, width: 3, height: 7 },
+        { id: "b", x: 3, y: 0, width: 5, height: 2 },
+        { id: "c", x: 3, y: 2, width: 4, height: 2 },
+        { id: "d", x: 10, y: 0, width: 6, height: 6 },
+      ],
+      newElement: { width: 3, height: 12 },
+    })
+  ).toStrictEqual({ x: 7, y: 2 });
+});
+
+test("Normal use case #1", () => {
   expect(
     getOptimalPosition({
       grid: { width: 16, height: 8 },
@@ -56,7 +86,7 @@ test("Regular use #1", () => {
   ).toStrictEqual({ x: 12, y: 0 });
 });
 
-test("Regular use #2", () => {
+test("Normal use case #2", () => {
   expect(
     getOptimalPosition({
       grid: { width: 16, height: 8 },
