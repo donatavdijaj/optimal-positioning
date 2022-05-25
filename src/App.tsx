07ba70ec5d647a028122e865c70ebe205f786b40
Donat Avdijaj <donatavdijaj@gmail.com>
2022-05-25 09:20:26 +0200

    Clean up boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
-import GridLayout from "react-grid-layout";
 import "/node_modules/react-grid-layout/css/styles.css";
 import "/node_modules/react-resizable/css/styles.css";
 
 function App() {
-  const layout = [
-    { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 },
-  ];
-
   return (
-    <div>
-      <GridLayout
-        className="layout"
-        layout={layout}
-        cols={12}
-        rowHeight={30}
-        width={1200}
-      >
-        <div key="a">a</div>
-        <div key="b">b</div>
-        <div key="c">c</div>
-      </GridLayout>
-    </div>
+    <>
+      <header>
+        <h1>Optimal Positioning by Donat Avdijaj</h1>
+      </header>
+      <main>
+        <p>
+          Algorithm for placing items inside of a grid, closest to the top.
+          <br />
+          <b>Check in 'src/utils/getOptimalPosition.ts'</b> for the algorithm,
+          <br />
+          and <b>'src/tests/getOptimalPosition.test.ts'</b> for the tests.
+        </p>
+      </main>
+    </>
   );
 }
 
