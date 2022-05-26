cd606c99f9f8193cf2cce65b7d37fe0484c02058
Donat Avdijaj <donatavdijaj@gmail.com>
2022-05-26 11:21:45 +0200

    Add form to add new elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "/node_modules/react-grid-layout/css/styles.css";
 import "/node_modules/react-resizable/css/styles.css";
 import GridLayout from "react-grid-layout";
 import { useState } from "react";
+import getOptimalPosition from "./utils/getOptimalPosition";
 
 function App() {
   const [layout, setLayout] = useState([
@@ -10,8 +11,33 @@ function App() {
     { i: "c", x: 4, y: 0, w: 1, h: 2 },
   ]);
 
+  const [newElemIdCounter, setNewElemIdCounter] = useState(1);
+  const [gridWith, setGridWith] = useState(16);
   const [widthInput, setWidthInput] = useState(2);
   const [heightInput, setHeightInput] = useState(4);
+
+  const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
+    // Preventing the page from reloading
+    event.preventDefault();
+    setNewElemIdCounter((currentValue) => {
+      return currentValue + 1;
+    });
+
+    const { x, y } = getOptimalPosition(
+      { width: gridWith, height: 8 },
+      layout,
+      {
+        w: widthInput,
+        h: heightInput,
+      }
+    );
+
+    // Do something
+    setLayout([
+      ...layout,
+      { i: "id" + newElemIdCounter, x: x, y: y, w: widthInput, h: heightInput },
+    ]);
+  };
 
   return (
     <>
@@ -19,19 +45,7 @@ function App() {
         <h1>Optimal Positioning by Donat Avdijaj</h1>
       </header>
       <main>
-        <GridLayout
-          className="layout"
-          layout={layout}
-          cols={12}
-          rowHeight={30}
-          width={1200}
-        >
-          {layout.map(({ i }) => {
-            return <div key={i}>{i}</div>;
-          })}
-        </GridLayout>
-
-        <form>
+        <form onSubmit={submitForm}>
           <label htmlFor="width">Width:</label>
           <input
             type="number"
@@ -51,14 +65,27 @@ function App() {
             value={heightInput}
             onChange={(e) => setHeightInput(e.target.valueAsNumber)}
           />
-          <button
-            onClick={() => {
-              setLayout([...layout, { i: "d", x: 8, y: 0, w: 1, h: 1 }]);
-            }}
-          >
-            Add item
-          </button>
+          <button type="submit">Add item</button>
         </form>
+
+        <GridLayout
+          className="layout"
+          layout={layout}
+          cols={gridWith}
+          rowHeight={30}
+          width={1500}
+          onLayoutChange={(newLayout) => {
+            console.log(newLayout.toString());
+          }}
+        >
+          {layout.map(({ i }) => {
+            return (
+              <div key={i} style={{ border: "solid" }}>
+                {i}
+              </div>
+            );
+          })}
+        </GridLayout>
       </main>
     </>
   );
