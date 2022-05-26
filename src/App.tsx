b69f05cd61fa01db294758a42e69b20e0e790d55
Donat Avdijaj <donatavdijaj@gmail.com>
2022-05-26 11:47:56 +0200

    Example to show off algorithm

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,29 @@ import "/node_modules/react-resizable/css/styles.css";
 import GridLayout from "react-grid-layout";
 import { useState } from "react";
 import getOptimalPosition from "./utils/getOptimalPosition";
+import "./App.css";
 
 function App() {
   const [layout, setLayout] = useState([
-    { i: "a", x: 0, y: 0, w: 1, h: 2 },
-    { i: "b", x: 1, y: 0, w: 3, h: 2 },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 },
+    { i: "a", x: 0, y: 0, w: 3, h: 7 },
+    { i: "b", x: 3, y: 0, w: 5, h: 2 },
+    { i: "c", x: 3, y: 2, w: 4, h: 2 },
+    { i: "d", x: 10, y: 0, w: 6, h: 6 },
   ]);
 
   const [newElemIdCounter, setNewElemIdCounter] = useState(1);
-  const [gridWith, setGridWith] = useState(16);
-  const [widthInput, setWidthInput] = useState(2);
-  const [heightInput, setHeightInput] = useState(4);
+  const [gridWith] = useState(16);
+  const [widthInput, setWidthInput] = useState(1);
+  const [heightInput, setHeightInput] = useState(1);
 
   const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
-    // Preventing the page from reloading
-    event.preventDefault();
+    event.preventDefault(); // don't reload page
     setNewElemIdCounter((currentValue) => {
       return currentValue + 1;
     });
 
     const { x, y } = getOptimalPosition(
-      { width: gridWith, height: 8 },
+      { width: gridWith, height: 25 },
       layout,
       {
         w: widthInput,
@@ -41,31 +42,38 @@ function App() {
 
   return (
     <>
-      <header>
+      <header id="header">
         <h1>Optimal Positioning by Donat Avdijaj</h1>
       </header>
-      <main>
-        <form onSubmit={submitForm}>
-          <label htmlFor="width">Width:</label>
-          <input
-            type="number"
-            name="width"
-            id="width"
-            min={0}
-            value={widthInput}
-            onChange={(e) => setWidthInput(e.target.valueAsNumber)}
-          />
-
-          <label htmlFor="height">Height:</label>
-          <input
-            type="number"
-            name="height"
-            id="height"
-            min={0}
-            value={heightInput}
-            onChange={(e) => setHeightInput(e.target.valueAsNumber)}
-          />
-          <button type="submit">Add item</button>
+      <main id="main">
+        <form onSubmit={submitForm} className="flex-center">
+          <div>
+            <div className="flex-center">
+              <label htmlFor="width">Width:</label>
+              <input
+                type="number"
+                name="width"
+                id="width"
+                min={0}
+                value={widthInput}
+                onChange={(e) => setWidthInput(e.target.valueAsNumber)}
+              />
+            </div>
+            <div className="flex-center">
+              <label htmlFor="height">Height:</label>
+              <input
+                type="number"
+                name="height"
+                id="height"
+                min={0}
+                value={heightInput}
+                onChange={(e) => setHeightInput(e.target.valueAsNumber)}
+              />
+            </div>
+          </div>
+          <button id="submit" type="submit">
+            Add item
+          </button>
         </form>
 
         <GridLayout
